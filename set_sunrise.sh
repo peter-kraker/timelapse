15c2e3214145501404b1e283ac30982127ec1dd9
Peter Kraker <pkraker@google.com>
2015-11-04 15:58:35 +0900

    Refactor
    Moved API key into an env variable. Trying not to leak keys.

--- set_sunrise.sh
+++ set_sunrise.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 TL=/usr/local/google/home/pkraker/timelapse
-JSON=`curl "http://api.wunderground.com/api/76d7dbccf8d73382/astronomy/q/Japan/Tokyo.json"`
+API=`cat wunderground_api_key`
+JSON=`curl "http://api.wunderground.com/api/$API/astronomy/q/Japan/Tokyo.json"`
 
 echo $JSON > $TL/tl_info.log 2>&1 $TL/wunderground.json
 
-function set_sunrise {
+function get_sunrise {
 	HOUR=`echo $JSON | jq ".sun_phase.sunrise.hour" | sed -e "s/\"//g"`
 	MINUTE=`echo $JSON | jq ".sun_phase.sunrise.minute" | sed -e "s/\"//g"` 
 	
-	echo "$HOUR:$MINUTE" > $TL/sunrise 
+	echo "$HOUR:$MINUTE"
 
 	HOUR=""
 	MINUTE=""
 }
 
-function set_sunset {
+function get_sunset {
 	HOUR=`echo $JSON | jq ".sun_phase.sunset.hour" | sed -e "s/\"//g"`
 	MINUTE=`echo $JSON | jq ".sun_phase.sunset.minute" | sed -e "s/\"//g"` 
 	
-	echo "$HOUR:$MINUTE" > $TL/sunset
+	echo "$HOUR:$MINUTE"
 
 	HOUR=""
 	MINUTE=""
 }
 
-set_sunrise
-set_sunset
+get_sunrise > $TL/sunrise
+get_sunset > $TL/sunset
