20223a155b3315efb7cce3f5791cdb757302f8f6
Peter Kraker <pkraker@google.com>
2016-03-24 18:05:28 +0900

    adding date selectors to uploads.sh

--- uploads.sh
+++ uploads.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
-for i in `seq 10 22`; do
-  ./upload.sh -m 01 -d $i -f mkv
+START=1
+END=31
+FORMAT="mkv"
+MONTH=`date +%m`
+
+while [[ $# >1 ]]
+do
+key="$1"
+
+case $key in
+  -m|--month)
+  if [[ "$2" == [1-9] ]]; then
+    MONTH=0$2
+  else
+    MONTH=$2
+  fi
+  shift
+  -f|--format)
+  FORMAT=$2
+  shift
+  ;;
+  -s|--start)
+  START=$2
+  shift
+  ;;
+  -e|--end)
+  END=$2
+  ;;
+  *)
+
+  ;;
+esac
+shift
+done
+
+for i in `seq $START $END`; do
+  ./upload.sh -m $MONTH -d $i -f $FORMAT 
 done
